f6f91975ed6d0974005dfcf54e220d99689f9789
Ali688273 <iiii688273@gmail.com>
2026-09-16 15:14:09 +0330

    Update GarageScreen.java

--- core/src/main/java/ir/motorush/game/screen/GarageScreen.java
+++ core/src/main/java/ir/motorush/game/screen/GarageScreen.java
@@ -11,8 +11,10 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import ir.motorush.game.MotoRushGame;
 import ir.motorush.game.game.BikeData;
 import ir.motorush.game.game.BikeRepository;
+import ir.motorush.game.game.EconomyConfig;
 import ir.motorush.game.game.PlayerProgress;
 import ir.motorush.game.game.ProgressManager;
+import ir.motorush.game.game.UpgradeType;
 
 import java.util.List;
 
@@ -33,6 +35,9 @@ public class GarageScreen implements Screen {
 
     private float width;
     private float height;
+
+    private String message = "";
+    private float messageTimer = 0f;
 
     public GarageScreen(
             MotoRushGame game
@@ -97,6 +102,10 @@ public class GarageScreen implements Screen {
         height =
                 Gdx.graphics.getHeight();
 
+        if (messageTimer > 0f) {
+            messageTimer -= delta;
+        }
+
         Gdx.gl.glClearColor(
                 0.025f,
                 0.035f,
@@ -147,10 +156,10 @@ public class GarageScreen implements Screen {
         );
 
         shapes.rect(
-                width * 0.08f,
-                height * 0.12f,
-                width * 0.84f,
-                height * 0.66f
+                width * 0.06f,
+                height * 0.08f,
+                width * 0.88f,
+                height * 0.82f
         );
 
         shapes.end();
@@ -161,120 +170,241 @@ public class GarageScreen implements Screen {
         BikeData bike =
                 bikes.get(selectedIndex);
 
+        String bikeId =
+                bike.getId();
+
+        boolean unlocked =
+                progress.isBikeUnlocked(
+                        bikeId
+                );
+
+        int speedLevel =
+                progressManager.getUpgradeLevel(
+                        bikeId,
+                        UpgradeType.SPEED
+                );
+
+        int handlingLevel =
+                progressManager.getUpgradeLevel(
+                        bikeId,
+                        UpgradeType.HANDLING
+                );
+
+        int nitroLevel =
+                progressManager.getUpgradeLevel(
+                        bikeId,
+                        UpgradeType.NITRO
+                );
+
         batch.begin();
 
         font.setColor(Color.WHITE);
 
-        font.getData().setScale(2.2f);
+        font.getData().setScale(2.0f);
 
         font.draw(
                 batch,
                 "GARAGE",
                 width * 0.38f,
-                height * 0.91f
-        );
-
-        font.getData().setScale(1.2f);
-
-        font.draw(
-                batch,
-                "COINS: "
-                        + progress.getCoins(),
-                width * 0.08f,
-                height * 0.84f
-        );
-
-        font.getData().setScale(1.7f);
-
-        font.draw(
-                batch,
-                bike.getName(),
-                width * 0.36f,
-                height * 0.70f
+                height * 0.94f
         );
 
         font.getData().setScale(1.15f);
 
         font.draw(
                 batch,
-                "SPEED   "
-                        + (int) bike.getSpeed(),
-                width * 0.20f,
-                height * 0.57f
+                "COINS: "
+                        + progress.getCoins(),
+                width * 0.08f,
+                height * 0.87f
+        );
+
+        font.getData().setScale(1.65f);
+
+        font.draw(
+                batch,
+                bike.getName(),
+                width * 0.35f,
+                height * 0.76f
+        );
+
+        font.getData().setScale(1.0f);
+
+        font.draw(
+                batch,
+                "SPEED       LV "
+                        + speedLevel,
+                width * 0.16f,
+                height * 0.65f
         );
 
         font.draw(
                 batch,
-                "HANDLING   "
-                        + (int) bike.getHandling(),
-                width * 0.20f,
-                height * 0.50f
+                "HANDLING    LV "
+                        + handlingLevel,
+                width * 0.16f,
+                height * 0.59f
         );
 
         font.draw(
                 batch,
-                "PRICE   "
-                        + bike.getPrice(),
-                width * 0.20f,
-                height * 0.43f
+                "NITRO       LV "
+                        + nitroLevel,
+                width * 0.16f,
+                height * 0.53f
         );
 
-        boolean unlocked =
-                progress.isBikeUnlocked(
-                        bike.getId()
-                );
+        if (!unlocked) {
 
-        font.getData().setScale(1.25f);
-
-        if (unlocked) {
+            font.getData().setScale(1.15f);
 
             font.draw(
                     batch,
-                    bike.getId()
-                            .equals(
-                                    progress
-                                            .getSelectedBikeId()
-                            )
-                            ? "SELECTED"
-                            : "TAP TO SELECT",
+                    "BUY: "
+                            + bike.getPrice()
+                            + " COINS",
                     width * 0.34f,
-                    height * 0.29f
+                    height * 0.43f
             );
 
         } else {
 
+            font.getData().setScale(1.0f);
+
             font.draw(
                     batch,
-                    "TAP TO BUY",
-                    width * 0.37f,
-                    height * 0.29f
+                    "SPEED UPGRADE: "
+                            + getUpgradeCostText(
+                            bikeId,
+                            UpgradeType.SPEED
+                    ),
+                    width * 0.12f,
+                    height * 0.43f
             );
+
+            font.draw(
+                    batch,
+                    "HANDLING UPGRADE: "
+                            + getUpgradeCostText(
+                            bikeId,
+                            UpgradeType.HANDLING
+                    ),
+                    width * 0.12f,
+                    height * 0.38f
+            );
+
+            font.draw(
+                    batch,
+                    "NITRO UPGRADE: "
+                            + getUpgradeCostText(
+                            bikeId,
+                            UpgradeType.NITRO
+                    ),
+                    width * 0.12f,
+                    height * 0.33f
+            );
+
+            if (
+                    bikeId.equals(
+                            progress
+                                    .getSelectedBikeId()
+                    )
+            ) {
+
+                font.draw(
+                        batch,
+                        "SELECTED",
+                        width * 0.42f,
+                        height * 0.27f
+                );
+
+            } else {
+
+                font.draw(
+                        batch,
+                        "TAP CENTER TO SELECT",
+                        width * 0.32f,
+                        height * 0.27f
+                );
+            }
         }
 
-        font.getData().setScale(1f);
+        font.getData().setScale(2f);
 
         font.draw(
                 batch,
                 "<",
-                width * 0.12f,
-                height * 0.30f
+                width * 0.08f,
+                height * 0.27f
         );
 
         font.draw(
                 batch,
                 ">",
-                width * 0.86f,
-                height * 0.30f
+                width * 0.88f,
+                height * 0.27f
         );
+
+        font.getData().setScale(0.95f);
 
         font.draw(
                 batch,
-                "TAP BOTTOM TO RETURN",
-                width * 0.32f,
+                "Tap bottom to return",
+                width * 0.36f,
                 height * 0.12f
         );
 
+        if (
+                messageTimer > 0f
+                        && !message.isEmpty()
+        ) {
+
+            font.setColor(
+                    Color.YELLOW
+            );
+
+            font.getData().setScale(1.15f);
+
+            font.draw(
+                    batch,
+                    message,
+                    width * 0.25f,
+                    height * 0.18f
+            );
+
+            font.setColor(
+                    Color.WHITE
+            );
+        }
+
         batch.end();
+    }
+
+    private String getUpgradeCostText(
+            String bikeId,
+            UpgradeType type
+    ) {
+
+        int level =
+                progressManager.getUpgradeLevel(
+                        bikeId,
+                        type
+                );
+
+        if (
+                level
+                        >= EconomyConfig.MAX_UPGRADE_LEVEL
+        ) {
+
+            return "MAX";
+        }
+
+        return String.valueOf(
+                EconomyConfig.getUpgradeCost(
+                        type,
+                        level
+                )
+        );
     }
 
     private void handleTouch() {
@@ -287,12 +417,26 @@ public class GarageScreen implements Screen {
                 Gdx.input.getX();
 
         float y =
-                height
-                        - Gdx.input.getY();
+                height - Gdx.input.getY();
+
+        if (
+                y < height * 0.16f
+        ) {
+
+            progressManager.save(
+                    progress
+            );
+
+            game.setScreen(
+                    new MainMenuScreen(game)
+            );
+
+            return;
+        }
 
         if (
                 y > height * 0.20f
-                        && y < height * 0.40f
+                        && y < height * 0.33f
         ) {
 
             if (
@@ -302,7 +446,6 @@ public class GarageScreen implements Screen {
                 selectedIndex--;
 
                 if (selectedIndex < 0) {
-
                     selectedIndex =
                             bikes.size() - 1;
                 }
@@ -327,26 +470,61 @@ public class GarageScreen implements Screen {
                 return;
             }
 
-            buyOrSelect();
+            selectOrBuyBike();
+
+            return;
+        }
+
+        BikeData bike =
+                bikes.get(selectedIndex);
+
+        if (
+                !progress.isBikeUnlocked(
+                        bike.getId()
+                )
+        ) {
+
+            buyBike();
 
             return;
         }
 
         if (
-                y < height * 0.18f
+                y > height * 0.38f
+                        && y < height * 0.46f
         ) {
 
-            progressManager.save(
-                    progress
+            upgrade(
+                    UpgradeType.SPEED
             );
 
-            game.setScreen(
-                    new MainMenuScreen(game)
+            return;
+        }
+
+        if (
+                y > height * 0.33f
+                        && y <= height * 0.38f
+        ) {
+
+            upgrade(
+                    UpgradeType.HANDLING
+            );
+
+            return;
+        }
+
+        if (
+                y > height * 0.28f
+                        && y <= height * 0.33f
+        ) {
+
+            upgrade(
+                    UpgradeType.NITRO
             );
         }
     }
 
-    private void buyOrSelect() {
+    private void selectOrBuyBike() {
 
         BikeData bike =
                 bikes.get(selectedIndex);
@@ -365,12 +543,39 @@ public class GarageScreen implements Screen {
                     progress
             );
 
+            showMessage(
+                    "BIKE SELECTED"
+            );
+
+            return;
+        }
+
+        buyBike();
+    }
+
+    private void buyBike() {
+
+        BikeData bike =
+                bikes.get(selectedIndex);
+
+        int price =
+                bike.getPrice();
+
+        if (
+                progress.getCoins()
+                        < price
+        ) {
+
+            showMessage(
+                    "NOT ENOUGH COINS"
+            );
+
             return;
         }
 
         if (
                 progress.spendCoins(
-                        bike.getPrice()
+                        price
                 )
         ) {
 
@@ -385,7 +590,100 @@ public class GarageScreen implements Screen {
             progressManager.save(
                     progress
             );
+
+            showMessage(
+                    "BIKE PURCHASED"
+            );
         }
+    }
+
+    private void upgrade(
+            UpgradeType type
+    ) {
+
+        BikeData bike =
+                bikes.get(selectedIndex);
+
+        String bikeId =
+                bike.getId();
+
+        if (
+                !progress.isBikeUnlocked(
+                        bikeId
+                )
+        ) {
+
+            showMessage(
+                    "BUY BIKE FIRST"
+            );
+
+            return;
+        }
+
+        int level =
+                progressManager.getUpgradeLevel(
+                        bikeId,
+                        type
+                );
+
+        if (
+                level
+                        >= EconomyConfig.MAX_UPGRADE_LEVEL
+        ) {
+
+            showMessage(
+                    "MAX LEVEL"
+            );
+
+            return;
+        }
+
+        int cost =
+                EconomyConfig.getUpgradeCost(
+                        type,
+                        level
+                );
+
+        if (
+                progress.getCoins()
+                        < cost
+        ) {
+
+            showMessage(
+                    "NOT ENOUGH COINS"
+            );
+
+            return;
+        }
+
+        if (
+                progress.spendCoins(
+                        cost
+                )
+        ) {
+
+            progressManager.saveUpgradeLevel(
+                    bikeId,
+                    type,
+                    level + 1
+            );
+
+            progressManager.save(
+                    progress
+            );
+
+            showMessage(
+                    "UPGRADE COMPLETE"
+            );
+        }
+    }
+
+    private void showMessage(
+            String text
+    ) {
+
+        message = text;
+        messageTimer = 1.5f;
     }
 
     @Override
