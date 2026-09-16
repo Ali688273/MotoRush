b806fd8e585deafb527a9259be9dbf65d3461644
Ali688273 <iiii688273@gmail.com>
2026-09-16 14:16:34 +0330

    Create AndroidLauncher.java

---
+++ android/src/main/java/ir/motorush/game/AndroidLauncher.java
@@ -0,0 +1,23 @@
+package ir.motorush.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+
+public class AndroidLauncher extends AndroidApplication {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        AndroidApplicationConfiguration configuration =
+                new AndroidApplicationConfiguration();
+
+        configuration.useImmersiveMode = true;
+        configuration.useAccelerometer = false;
+        configuration.useCompass = false;
+
+        initialize(new MotoRushGame(), configuration);
+    }
+}
